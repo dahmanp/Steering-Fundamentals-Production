66ef31bd7629e7daed587e405e68e0ff529f83df
dahmanp <dahmanp@my.erau.edu>
2025-01-25 13:53:00 -0700

    Added Pursuer and Evader, Labels to the Objects for clarity

---
+++ Steering Starter Project/Assets/Scripts/PathFinder.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder : Kinematic
+{
+    /*FollowPath myMoveType;
+    LookWhereGoing myRotateType;
+    GameObject[] myPath;
+
+    void Start()
+    {
+        myMoveType = new FollowPath();
+        myMoveType.character = this;
+        myMoveType.path = myPath;
+
+        myRotateType = new LookWhereGoing();
+        myRotateType.character = this;
+        myRotateType.target = myTarget;
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        steeringUpdate = new SteeringOutput();
+        steeringUpdate.angular = myRotateType.getSteering().angular;
+        steeringUpdate.linear = myMoveType.getSteering().linear;
+        base.Update();
+    }*/
+}
